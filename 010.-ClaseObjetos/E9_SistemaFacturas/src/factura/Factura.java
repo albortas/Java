c2a42bd0d29509af3cd30c0f39d9e117fcc3b692
albortas <garlad628@gmail.com>
2024-07-20 20:21:16 -0400

    Añadiendo el sistema de facturas de la clase objeto

--- 010.-ClaseObjetos/E9_SistemaFacturas/src/factura/Factura.java
+++ 010.-ClaseObjetos/E9_SistemaFacturas/src/factura/Factura.java
@@ -1,5 +1,63 @@
 package factura;
 public class Factura {
-
     //Atributos
+    private int numeroPieza;
+    private String descripcionPieza;
+    private int cantidadComprar;
+    private double precioArticulo;
+
+    //Constructor
+    public Factura(int numeroPieza, String descripcionPieza, int cantidadComprar, double precioArticulo){
+        this.numeroPieza = numeroPieza;
+        this.descripcionPieza = descripcionPieza;
+        if(cantidadComprar > 0)
+            this.cantidadComprar = cantidadComprar;
+        if(precioArticulo > 0.0)
+            this.precioArticulo = precioArticulo;
+    }
+
+    //Metodo get (Mostrar atributos)
+    public int getNumeroPieza(){
+        return this.numeroPieza;
+    }
+
+    public String getDescripcionPieza(){
+        return this.descripcionPieza;
+    }
+
+    public int getCantidadComprar(){
+        return this.cantidadComprar;
+    }
+
+    public double getPrecioArticulo(){
+        return this.precioArticulo;
+    }
+
+    //Metodos set (Modificar atributos)
+    public void setNumeroPieza(int numeroPieza){
+        this.numeroPieza = numeroPieza;
+    }
+
+    public void setDescripcionPieza(String descripcionPieza){
+        this.descripcionPieza = descripcionPieza;
+    }
+
+    public void setCantidadComprar(int cantidadComprar){
+        this.cantidadComprar = cantidadComprar;
+    }
+
+    public void setPrecioArticulo(double precioArticulo){
+        this.precioArticulo = precioArticulo;
+    }
+
+    //Metodos
+    public double montoFactura(){
+
+        double montoFactura = this.cantidadComprar * this.precioArticulo;
+        return montoFactura;
+    }
+
+    public void mostrarDatos(){
+        System.out.println();
+    }
 }
